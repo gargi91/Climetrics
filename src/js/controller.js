e7111f84d75e6a84362f7732b405290efa74ad81
Gargi Chaurasia <gargi.0901@gmail.com>
2021-08-29 15:20:04 +0530

    Functionality init

--- src/js/controller.js
+++ src/js/controller.js
@@ -19,28 +19,28 @@ import ReportView from "./views/reportView";
 // // window.addEventListener("load", init);
 // init();
 
-const getUserLocation = async function () {
-	try {
-		const { coords } = await model.getPosition();
-		const { latitude, longitude } = coords;
-		model.state.location.lat = latitude;
-		model.state.location.lng = longitude;
-		console.log(model.state);
-	} catch (error) {
-		console.error(`😡😡😡 Abe location ni mil raha!`);
-		console.log("Ab default use kro...");
-		console.log(model.state);
-	}
-};
-
-//Controlling the report view
-const timeout = function (s) {
-	return new Promise(function (resolve, _) {
-		setTimeout(function () {
-			resolve(`Request took too long! Timeout after second`);
-		}, 1);
-	});
-};
+// const getUserLocation = async function () {
+// 	try {
+// 		const { coords } = await model.getPosition();
+// 		const { latitude, longitude } = coords;
+// 		model.state.location.lat = latitude;
+// 		model.state.location.lng = longitude;
+// 		console.log(model.state);
+// 	} catch (error) {
+// 		console.error(`😡😡😡 Abe location ni mil raha!`);
+// 		console.log("Ab default use kro...");
+// 		console.log(model.state);
+// 	}
+// };
+//
+// //Controlling the report view
+// const timeout = function (s) {
+// 	return new Promise(function (resolve, _) {
+// 		setTimeout(function () {
+// 			resolve(`Request took too long! Timeout after second`);
+// 		}, 1);
+// 	});
+// };
 
 window.addEventListener("load", () => {
 	getUserLocation();
