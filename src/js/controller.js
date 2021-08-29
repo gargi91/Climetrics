92fb4dcaa946bafd3344e9087e9d8372506097ea
Gargi Chaurasia <gargi.0901@gmail.com>
2021-08-29 14:52:59 +0530

    new

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,19 +1,47 @@
 "use strict";
 import Chart from "chart.js/auto";
+import ChartView from "./views/chartView";
 import * as model from "./model.js";
 import CitiesView from "./views/citiesView";
 import ReportView from "./views/reportView";
 
-const init = async function () {
+// const init = async function () {
+// 	try {
+// 		ReportView.renderSpinner();
+// 		await model.loadData();
+// 		ReportView.render(model.state);
+// 		ReportView.updateUI(model.state.current);
+// 		CitiesView.render(model.state.topCities);
+// 	} catch (err) {
+// 		console.error(`🥵😱 ${err}`);
+// 	}
+// };
+// // window.addEventListener("load", init);
+// init();
+
+const getUserLocation = async function () {
 	try {
-		ReportView.renderSpinner();
-		await model.loadData();
-		ReportView.render(model.state);
-		ReportView.updateUI(model.state.current);
-		CitiesView.render(model.state.topCities);
-	} catch (err) {
-		console.error(`🥵😱 ${err}`);
+		const { coords } = await model.getPosition();
+		const { latitude, longitude } = coords;
+		model.state.location.lat = latitude;
+		model.state.location.lng = longitude;
+		console.log(model.state);
+	} catch (error) {
+		console.error(`😡😡😡 Abe location ni mil raha!`);
+		console.log("Ab default use kro...");
+		console.log(model.state);
 	}
 };
-// window.addEventListener("load", init);
-init();
+
+//Controlling the report view
+const timeout = function (s) {
+	return new Promise(function (resolve, _) {
+		setTimeout(function () {
+			resolve(`Request took too long! Timeout after second`);
+		}, 1);
+	});
+};
+
+window.addEventListener("load", () => {
+	getUserLocation();
+});
